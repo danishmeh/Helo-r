15da2b138cca24520f8d73631990c69410fe31a6
Danish <danishmehmood1887@gmail.com>
2022-03-21 12:24:51 +0500

    My info

---
+++ myinfo.r
@@ -0,0 +1,5 @@
+#My information
+print("My Information")
+print("I am Danish Mehmood from Pakistan")
+Print("Learning R programing from Alnafi online Institute")
+print("R Programing Teacher is Miss Sana Rasheed")
